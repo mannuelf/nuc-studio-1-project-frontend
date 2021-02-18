b1a0a29c5bd78d12b1c49dbd92b9099bb6c88677
srdjanf <44509446+srdjanf@users.noreply.github.com>
2021-02-18 18:55:52 +0100

    Small changes

--- src/main.js
+++ src/main.js
@@ -41,3 +41,35 @@ var l_encurso = L.geoJson(<%- ordenesl %>, {
     },
     onEachFeature: onEachFeature
 }); */
+
+var geojson;
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
