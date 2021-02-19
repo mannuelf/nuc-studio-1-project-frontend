55a9f5f6020356598bbd316842776a1f45cd1dab
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-02-19 14:55:33 +0100

    make modules for readability

--- src/js/scripts.ts
+++ src/js/scripts.ts
@@ -1,10 +1,5 @@
 import * as L from "leaflet/dist/leaflet-src.esm";
-
-const myToken = `pk.eyJ1IjoibWFubnVlbGYiLCJhIjoiY2tsYzZvNDZsMG05ZjJwcDB1OGg0MXJ2NiJ9.g5VNTSuAnyZ45zg9lsZgOg`;
-const config = {
-  apiMapBoxUrl: `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${myToken}`,
-  mapBoxAttribution: `Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>`,
-};
+import { config, myToken } from "./config";
 
 const mainMap = L.map("map").setView([40, 10], 4);
 
