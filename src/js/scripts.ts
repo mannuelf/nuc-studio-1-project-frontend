fca28e61cdb42a2be0c01b675e70a2421b860cc1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-06 21:46:05 +0200

    showing usa data on mouse over shows data

--- src/js/scripts.ts
+++ src/js/scripts.ts
@@ -1,18 +1,78 @@
-import * as L from "leaflet/dist/leaflet-src.esm";
-import { config, myToken } from "./config";
+import * as L from 'leaflet/dist/leaflet-src.esm';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
+import { config, myToken } from './config';
 
-const mainMap = L.map("map").setView([40, 10], 4);
+mapboxgl.accessToken = myToken;
 
-console.log(typeof mainMap, mainMap);
-// the site auto reloads after you type
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
 
-L.tileLayer(`${config.apiMapBoxUrl}`, {
-  attribution: config.mapBoxAttribution,
-  maxZoom: 18,
-  id: "mapbox/streets-v11",
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: `${myToken}`,
-}).addTo(mainMap);
+map.on('load', () => {
+  // make a pointer cursor
+  map.getCanvas().style.cursor = 'default';
 
-// add all your code here.
+  // set map bounds to the continental US
+  map.fitBounds([
+    [-133.2421875, 16.972741],
+    [-47.63671875, 52.696361],
+  ]);
+
+  map.getCanvas().style.cursor = 'default';
+
+  const layers = [
+    '0-10',
+    '10-20',
+    '20-50',
+    '50-100',
+    '100-200',
+    '200-500',
+    '500-1000',
+    '10000+',
+  ];
+
+  const colors = [
+    '#FFEDA0',
+    '#FED976',
+    '#FEB24C',
+    '#FD8D3C',
+    '#FC4E2A',
+    '#E31A1C',
+    '#BD0026',
+    '#800026',
+  ];
+
+  // add legend
+  for (let i = 0; i < layers.length; i++) {
+    const layer = layers[i];
+    const color = colors[i];
+    const item = document.createElement('div');
+    const key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
+
+    let value = document.createElement('span');
+    value.innerHTML = layer;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+  }
+
+  // change info window on hover
+  map.on('mousemove', (e) => {
+    const states = map.queryRenderedFeatures(e.point, {
+      layers: ['statedata'],
+    });
+    if (states.length > 0) {
+      document.getElementById('pd').innerHTML =
+        '<h3><strong>' +
+        states[0].properties.name +
+        '</strong></h3><p><strong><em>' +
+        states[0].properties.density +
+        '</strong> people per square mile</em></p>';
+    } else {
+      document.getElementById('pd').innerHTML = '<p>Hover over a state!</p>';
+    }
+  });
+});
