d838fd9455957762f22d12b957defd7cd52f1e0c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-11 20:04:14 +0200

    fix searchbar

--- src/js/utils/searchBar.ts
+++ src/js/utils/searchBar.ts
@@ -1,19 +1,24 @@
+import _ from 'lodash';
 import axios from 'axios';
-import {MAPBOX_TOKEN, ENDPOINT_GEOCODING} from '../config/constants';
+import { ENDPOINT_GEOCODING, MAPBOX_TOKEN } from '../config/constants';
 
-export default async function getBboxCoordinates() {
-  let bbox = []; // mapboxs name for gpscoords
-  const searchForm = document.querySelector('#searchBar'); // get the search bar
+export const searchBar = async () => {
+  const searchForm = document.querySelector('#searchBar') as HTMLFormElement;
+  const searchInput = document.querySelector(
+    '#searchBar .input'
+  ) as HTMLInputElement;
 
-  searchForm.addEventListener('submit', async (e: any): Promise<IbboxGps> => {
+  searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const [searchInput] = e.currentTarget;
-    const userInput = searchInput.value.toLowerCase().replace('', '_');
-    const response = await axios.get(`${ENDPOINT_GEOCODING}/${userInput}.json?limit=2&access_token=${MAPBOX_TOKEN}`); // api call to mapbox
-    const [bboxCoordinates] = response.data.features; // get pgs coord out of api
-    searchInput.value = ''; // clear the input text
-    bbox = bboxCoordinates.bbox;
-  }
-  return bbox;
-}
+  });
 
+  searchInput.onkeyup = _.debounce(async (e) => {
+    console.log('searching');
+    const { value } = e.target;
+    const userInput = value.toLowerCase().trim().replace('', '_');
+    const mapboxPlace = await axios.get(
+      `${ENDPOINT_GEOCODING}/${userInput}.json?limit=2&access_token=${MAPBOX_TOKEN}`
+    );
+    return mapboxPlace;
+  }, 400);
+};
