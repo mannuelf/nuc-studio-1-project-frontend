55a725ad2c331bf7809c6d3e6ad2b49395ad5778
srdjanf <44509446+srdjanf@users.noreply.github.com>
2021-04-30 03:25:42 +0200

    New country polygons and some suggestions and problems
    I hope performance is not affected by 150000 lines of JSON.

--- src/js/models/getPolygon.ts
+++ src/js/models/getPolygon.ts
@@ -9,9 +9,28 @@ export default function getPolygon(country: Country): any {
     lesotho: Polygon.LESOTHO,
     luxembourg: Polygon.LUXEMBOURG,
     norway: Polygon.NORWAY,
-    'south-africa': Polygon.SOUTHAFRICA,
+    /* Becuase of the MapBox we can not use a dash between South and Afrika
+     * Is it possible to change the object
+     * name inside of JSON to "south afrika"?
+     * Same problem with other 2+ name Countries
+     */
+    'south africa': Polygon.SOUTHAFRICA,
     sweden: Polygon.SWEDEN,
     greenland: Polygon.GREENLAND,
+    andorra: Polygon.ANDORRA,
+    russia: Polygon.RUSSIA,
+    montenegro: Polygon.MONTENEGRO,
+    india: Polygon.INDIA,
+    austria: Polygon.AUSTRIA,
+    brazil: Polygon.BRAZIL,
+    canada: Polygon.CANADA,
+    'united states': Polygon.UNITEDSTATES,
+    mexico: Polygon.MEXICO,
+    belgium: Polygon.BELGIUM,
+    chile: Polygon.CHILE,
+    china: Polygon.CHINA,
+    denmark: Polygon.DENMARK,
+    france: Polygon.FRANCE,
   };
 
   const polygonShape = Object.keys(COUNTRIES)
